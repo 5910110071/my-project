31d17917e1cfcad0bf3dcb9b4895e87c10bf0964
sudatiptaweesuk <icesudatip_taweesuk@hotmail.com>
2018-09-02 21:49:41 +0700

    by keem

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
 	printf("Test\n");
-	printf("Hello World\n 1234");
-	printf("Hi my name is ice");
-	printf("Hi my name is non");
+	printf("Hello World\n 1234\n");
+	printf("Hi I am Ice");
+	printf("Hi I am Keem");
 	return 0;
 }
